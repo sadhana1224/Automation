0578d6a21c0875be0dd01b184b1f3db392a3acaf
sadhana <sadhu.lokii1924@gmail.com>
2023-07-24 15:35:09 +0530

    selenium programs

---
+++ src/test/java/com/selenium/basics/PrintAllLinks.java
@@ -0,0 +1,44 @@
+package com.selenium.basics;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PrintAllLinks {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+
+        driver.manage().window().maximize();
+
+        WebElement sb=driver.findElement(By.xpath("//textarea[@id='APjFqb']"));
+        sb.sendKeys("Selenium IDE");
+        sb.sendKeys(Keys.ENTER);
+
+		Thread.sleep(1000);
+		
+		List<WebElement> links= driver.findElements(By.tagName("a"));
+		
+		//for(Datatype variablename:collection)
+		for (WebElement eachLink:links)
+		{
+			System.out.println(eachLink.getAttribute("href"));
+		}
+		
+		Thread.sleep(2000);
+		
+		driver.quit();
+		
+		
+
+	}
+
+}
